dfdedfda7768d8e9505ae3173dcecc25a62eed28
AlexandrVN <alexandr.novohatko@gmail.com>
2018-12-16 15:06:00 +0200

    Added databases

--- src/main/java/com/controller/HomeController.java
+++ src/main/java/com/controller/HomeController.java
@@ -1,14 +1,21 @@
 package com.controller;
 
+import com.entity.Message;
+import com.repository.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    private MessageRepository messageRepository;
 
     @GetMapping(value = {"/", "/index"})
     public String index(
@@ -24,6 +31,29 @@ public class HomeController {
             //для вывода статистической страницы необходимо указывать полный путь: "/webapp/WEB-INF/eco-app/index.mustache"
             return new ModelAndView("index");
     }*/
+
+    @GetMapping
+    public String main(Map<String,Object> model){
+        Iterable<Message> messages = messageRepository.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping
+    public String add(
+            @RequestParam String text,
+            @RequestParam String tag,
+            Map<String, Object> model){
+
+        Message message = new Message(text, tag);
+        messageRepository.save(message);
+
+        Iterable<Message> messages = messageRepository.findAll();
+        model.put("messages", messages);
+
+        return "main";
+    }
+
 
 
 }
