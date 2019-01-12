f5382135f487e870c7343c6883d7f3e6f36c24f8
AlexandrVN <alexandr.novohatko@gmail.com>
2019-01-12 14:47:58 +0200

    fixed security

--- src/main/java/com/config/WebSecurityConfig.java
+++ src/main/java/com/config/WebSecurityConfig.java
@@ -1,18 +1,21 @@
 package com.config;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -21,7 +24,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 //включаем авторизацию
                 .authorizeRequests()
                     //пользователи, который приходят по этому пути имеют полный доступ
-                    .antMatchers("/").permitAll()
+                    .antMatchers("/", "/registration").permitAll()
                     //для других запросов требуем авторизацию
                     .anyRequest().authenticated()
                 .and()
@@ -38,7 +41,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     }
 
-    @Bean
+/*    @Bean
     @Override
     protected UserDetailsService userDetailsService() {
 
@@ -50,7 +53,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                         .build();
 
         return new InMemoryUserDetailsManager(user);
-    }
+    }*/
 
-
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                .usersByUsernameQuery("select username, password, active from user_db where username=?")
+                .authoritiesByUsernameQuery("select u.username, ur.roles from user_db u inner join user_role ur on u.id = ur.user_id where u.username=?");
+        }
 }
